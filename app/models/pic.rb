510ea80186e34f07a45eb415b4ae04fede7cfbf5
rufunt <jazzoo@mail.ru>
2018-01-31 16:51:21 +0300

    Add acts_as_votable gem and setup

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,7 +1,8 @@
 class Pic < ApplicationRecord
+  acts_as_votable
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300", thumb: "100x100" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "300x300>" }
   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
